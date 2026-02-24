6e97e24d0fe7a122e1f4b6ceb1bef058e464d51a
TemurMalik <tmaq77@gmail.com>
2026-02-24 14:42:08 +0500

    Add Reame

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Navbar from "./Navbar";
-
+import { useTranslation } from "react-i18next";
 function App() {
+  let { t } = useTranslation();
+
   return (
     <>
       <Navbar />
+      <h1>{t("hi")}</h1>
+      <h2>{t("us")}</h2>
     </>
   );
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const Navbar = () => {
+  const { i18n } = useTranslation();
   return (
     <div>
-      <h1>Navbar</h1>
+      <button onClick={() => i18n.changeLanguage("uz")}>uz</button>
+      <button onClick={() => i18n.changeLanguage("ru")}>ru</button>
+      <button onClick={() => i18n.changeLanguage("en")}>en</button>
     </div>
   );
 };
